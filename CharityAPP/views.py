017a1b8d01f8d4730a3cb25cecbf88526d27d2f1
Piotrek <piotrg3378@wp.pl>
2024-03-19 15:15:11 +0100

    changes

--- CharityAPP/views.py
+++ CharityAPP/views.py
@@ -1,11 +1,16 @@
+from django.db.models import Sum
 from django.shortcuts import render
 from django.views import View
+
+from CharityAPP.models import Institution, Donation
 
 
 # Create your views here.
 class LandingPage(View):
+    bags_count =  Donation.objects.aggregate(total_quantity=Sum('quantity'))['total_quantity'] or 0
+    organizations_count = Donation.objects.count()
     def get(self, request):
-        return render(request, 'index.html')
+        return render(request, 'index.html', {'bags_count': self.bags_count, 'organizations_count': self.organizations_count})
 
 
 class AddDonation(View):
